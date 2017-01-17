b4d60ef3f53e162771d694b52223b319e5028d95
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2017-01-17 00:00:35 +0000

    Updated

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import os
 import sys
 import gzip
 import logging
-import model_keys_utils
+from keys_server.KeysLookup import KeysLookup
 from flask import Flask, Response, request
 from oasis_utils import oasis_utils, oasis_log_utils
 
@@ -29,19 +29,19 @@ CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(inspect.getfile(inspect.curr
 INI_PATH = os.path.abspath(os.path.join(CURRENT_DIRECTORY, 'KeysServer.ini'))
 CONFIG_PARSER.read(INI_PATH)
 
+# Logging configuration
+oasis_log_utils.read_log_config(CONFIG_PARSER)
+
 DO_GZIP_RESPONSE = CONFIG_PARSER.getboolean('Default', 'DO_GZIP_RESPONSE')
 PORT = CONFIG_PARSER.get('Default', 'PORT')
 KEYS_DATA_DIRECTORY = '/var/oasis/keys_data'
-
-# Logging configuration
-oasis_log_utils.read_log_config(CONFIG_PARSER)
 
 # Load the keys data
 logging.getLogger().info("Starting load keys data.")
 model_version_file = os.path.join(KEYS_DATA_DIRECTORY, 'ModelVersion.csv')
 if not os.path.isdir(KEYS_DATA_DIRECTORY):
     logging.getLogger().exception(
-        "Keys data dir not found: {}".format(KEYS_DATA_DIRECTORY))
+        "Keys data directory not found: {}".format(KEYS_DATA_DIRECTORY))
     exit()
 if not os.path.isfile(model_version_file):
     logging.getLogger().exception(
@@ -53,7 +53,14 @@ with open(model_version_file) as f:
     logging.getLogger().info("Supplier: {}".format(SUPPLIER))
     logging.getLogger().info("Model name: {}".format(MODEL_NAME))
     logging.getLogger().info("Model version: {}".format(MODEL_VERSION))
-utility.init(KEYS_DATA_DIRECTORY)
+
+try:
+    keys_lookup = KeysLookup()
+    keys_lookup.init(KEYS_DATA_DIRECTORY)
+except:
+    logging.getLogger().exception("Error initializing lookup")
+    exit()
+
 
 @oasis_log_utils.oasis_log()
 @APP.route(
@@ -65,6 +72,26 @@ def get_healthcheck():
     '''
     return "OK"
 
+def _check_content_type():
+    """
+    Check that the POST data is CSV.
+    """
+    content_type = ''
+    if 'Content-Type' in request.headers:
+        content_type = request.headers['Content-Type']
+    if content_type != 'text/csv; charset=utf-8':
+        raise Exception("Unsupported content type: ", content_type)
+
+
+def _is_gzipped():
+    """
+    Is the POST data gzipped?
+    """
+    is_gzipped = False
+    if 'Content-Encoding' in request.headers:
+        content_encoding = request.headers['Content-Encoding']
+        is_gzipped = (content_encoding == 'gzip')
+    return is_gzipped
 
 @oasis_log_utils.oasis_log()
 @APP.route(
@@ -78,20 +105,9 @@ def post_get_keys():
     try:
         lookup_results = list()
 
-        content_type = ''
-        if 'Content-Type' in request.headers:
-            content_type = request.headers['Content-Type']
-
-        is_gzipped = False
-
-        if 'Content-Encoding' in request.headers:
-            content_encoding = request.headers['Content-Encoding']
-            is_gzipped = (content_encoding == 'gzip')
-
-        if content_type == 'text/csv; charset=utf-8':
-            lookup_results = process_csv(is_gzipped)
-        else:
-            raise Exception("Unsupported content type: ", content_type)
+        _check_content_type()
+        is_gzipped = _is_gzipped()
+        lookup_results = process_csv(is_gzipped)
 
         response_data = {
             "status": 'success',
@@ -103,13 +119,15 @@ def post_get_keys():
         if DO_GZIP_RESPONSE:
             data = gzip.zlib.compress(data)
 
-        response = Response(data, status=200, mimetype="application/json")
+        response = Response(
+            data, status=oasis_utils.HTTP_RESPONSE_OK, mimetype="application/json")
 
         if DO_GZIP_RESPONSE:
             response.headers['Content-Encoding'] = 'gzip'
     except:
         logging.getLogger().exception("Error in post_lookup")
-        response = Response(status=500)
+        response = Response(
+            status=oasis_utils.HTTP_RESPONSE_INTERNAL_SERVER_ERROR)
     return response
 
 
@@ -127,15 +145,13 @@ def process_csv(is_gzipped):
     logging.getLogger().debug("Processing locations - csv")
 
     results = list()
-
     reader = csv.reader(io.StringIO(data), delimiter=',')
 
     #Skip the header
     next(reader)
-
     processed_count = 0
     for row in reader:
-        utility.process_row(row, results)
+        keys_lookup.process_row(row, results)
         processed_count = processed_count + 1
         if processed_count % 100 == 0:
             logging.info("Processed {} locations".format(processed_count))
