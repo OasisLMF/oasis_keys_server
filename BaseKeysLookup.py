2fd62678910228510b215b5f5725eb39b1b56aab
Sandeep Murthy <smurthy@protonmail.ch>
2017-03-09 16:13:52 +0000

    Removed redundant methods from BaseKeysLookup

--- BaseKeysLookup.py
+++ BaseKeysLookup.py
@@ -11,16 +11,6 @@ class BaseKeysLookup(object):
     Base implementation of model keys lookup logic.
     """
 
-    peril_id = oasis_utils.UNKNOWN_ID
-
-    @oasis_log_utils.oasis_log()
-    def init(self, keys_data_directory):
-        """
-        Initialise the static data required for the lookup.
-        """
-        pass
-
-
     @oasis_log_utils.oasis_log()
     def __init__(
         self,
@@ -52,45 +42,6 @@ class BaseKeysLookup(object):
         pass
 
 
-    @oasis_log_utils.oasis_log()
-    def process_row(self, row, results):
-        """
-        Process a location row, and add the results of the
-        lookup to the results.
-        """
-        record = None
-        row_failed = False
-        for coverage_type in (
-                oasis_utils.BUILDING_COVERAGE_CODE,
-                oasis_utils.CONTENTS_COVERAGE_CODE):
-            try:
-                if record is None:
-                    record = self._read_record(row)
-                record['coverage_type'] = coverage_type
-                ap_id, area_peril_message = self._get_area_peril_id(record)
-                vul_id, vulnerability_message = self._get_vulnerability_id(record)
-            except:
-                row_failed = True
-                logging.exception("Error processing row: {}".format(row))
-
-            if row_failed:
-                status = oasis_utils.KEYS_STATUS_FAIL
-            else:
-                status = self._get_lookup_success(ap_id, vul_id)
-
-            exposure_record = {
-                'id': record['loc_id'],
-                'peril_id': oasis_utils.PERIL_ID_QUAKE,
-                'coverage': coverage_type,
-                'area_peril_id': self.peril_id,
-                'vulnerability_id': vul_id,
-                'message': "{} - {}".format(
-                    area_peril_message, vulnerability_message),
-                'status': status
-            }
-            results.append(exposure_record)
-
-
     def _get_location_record(self, raw_loc_item):
         """
         Returns a dict of standard location keys and values based on
@@ -99,34 +50,6 @@ class BaseKeysLookup(object):
         tuple.
         """
         pass
-
-
-    def _read_record(self, line):
-        """
-        Parse a line from the CSV data
-        """
-        vals = [x.strip().upper() for x in line]
-        vals.reverse()
-        return {
-            'loc_id': self._to_int(vals.pop()),
-            'accntnum': vals.pop().strip(),
-            'locnum': self._to_int(vals.pop()),
-            'city': vals.pop().strip(),
-            'state': vals.pop().strip(),
-            'latitude': self._to_float(vals.pop()),
-            'longitude': self._to_float(vals.pop()),
-            'addrmatch': self._to_int(vals.pop()),
-            'country': vals.pop().strip(),
-            'numbldgs': self._to_int(vals.pop()),
-            'bldgscheme': vals.pop().strip(),
-            'bldgclass': vals.pop().strip(),
-            'occscheme': vals.pop().strip(),
-            'occtype': self._to_int(vals.pop()),
-            'cntryscheme': vals.pop().strip(),
-            'cntrycode': vals.pop().strip(),
-            'cv1val': self._to_float(vals.pop()),
-            'cv2val': self._to_float(vals.pop()),
-        }
 
 
     def _get_area_peril_id(self, record):
