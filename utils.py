0df6e54204dd0c7dade1f331f95d071f1f256ddb
Sandeep Murthy <smurthy@protonmail.ch>
2018-02-20 10:21:31 +0000

    Update Flask app utils module - rename supplier and model vars.

--- utils.py
+++ utils.py
@@ -15,17 +15,17 @@ from oasis_utils import (
 @oasis_log_utils.oasis_log()
 def get_keys_lookup_instance(
     keys_data_directory,
-    supplier,
-    model_name,
-    model_version
+    supplier_id,
+    model_id,
+    model_version_id
 ):
     """
-    Utility method to create a keys lookup instance for the given supplier,
-    model name and version parameters.
+    Utility method to create a keys lookup instance for the given supplier ID,
+    model ID and model version ID.
     """
     try:
-        klc = getattr(keys_server, "{}KeysLookup".format(model_name))
+        klc = getattr(keys_server, "{}KeysLookup".format(model_id))
     except AttributeError as e:
         raise OasisException(e)
 
-    return klc(keys_data_directory, supplier, model_name, model_version)
+    return klc(keys_data_directory, supplier_id, model_id, model_version_id)
