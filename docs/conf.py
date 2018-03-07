6dc30add85f2a7658a568d6cba2d627995c6584a
Sandeep Murthy <smurthy@protonmail.ch>
2018-03-07 13:04:28 +0000

    Add Sphinx `httpdomain` extension to `conf.py`

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(cwd, '..', '..')))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.httpdomain',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
