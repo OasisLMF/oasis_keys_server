1038c2c6ade0caa0b05c7874930c705af57b2331
sambles <sambles@users.noreply.github.com>
2018-07-09 10:23:49 +0100

    Update APP - Bring into line with OasisLMF v1.1.20

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ from flask import (
 from werkzeug.exceptions import HTTPException
 
 # Oasis libraries
-from oasislmf.keys.lookup import OasisKeysLookupFactory as oklf
+from oasislmf.keys.lookup import OasisLookupFactory as oklf
 from oasislmf.utils.compress import compress_data
 from oasislmf.utils.conf import load_ini_file
 from oasislmf.utils.exceptions import OasisException
