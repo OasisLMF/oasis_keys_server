2c8af385f579bf7c618b4ad1137a1e04278d4085
sam gamble <hexadessa@gmail.com>
2018-08-07 23:12:27 +0100

    Fix log mount issue - CI feature

--- startup.sh
+++ startup.sh
@@ -2,5 +2,6 @@
 APACHE_UID=$(id -g www-data)
 chown -R $APACHE_UID /var/oasis/keys_data/
 
+mkdir -p /var/log/oasis/ && touch /var/log/oasis/keys_server.log
 apache2ctl start 
 tail -f /var/log/oasis/keys_server.log
