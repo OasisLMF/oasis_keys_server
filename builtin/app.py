734a7f7543ae162ce5a5619ea6a802ac961c330e
S Murthy <smurthy@protonmail.ch>
2018-09-28 15:18:00 +0100

    Fix typo in built-in Flask app

--- builtin/app.py
+++ builtin/app.py
@@ -220,7 +220,7 @@ def get_keys():
 
         logging.info('Calling model lookup {} to generate keys ...'.format(oasis_lookup.__class__))
 
-        res_str =  ''.join(['{},'.format(json.dumps(r)) for r in keys_lookup.process_locations(loc_df)])
+        res_str =  ''.join(['{},'.format(json.dumps(r)) for r in oasis_lookup.bulk_lookup(loc_df)])
         res_str = res_str.rstrip(',')
         res_str = '{{"status":"success","items":[{}]}}'.format(''.join(res_str))
 
